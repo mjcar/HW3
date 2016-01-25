fe74ddca0cbf36c2d5e9e170b647a42b6763dd84
mjcar <mjcar@umich.edu>
2016-01-25 11:31:19 -0500

    Update list.c

--- list.c
+++ list.c
@@ -8,17 +8,62 @@
 // `index` member in ascending order. Returns a pointer to the head of the list
 // after the insertion of the new element.
 list_t* insert_sorted(list_t* head, list_t* new_element) {
-	assert(head != NULL);
-	assert(new_element != NULL);
-
-	return head;
+    assert(head != NULL);
+    assert(new_element != NULL);
+    list_t* prev, curr;
+    uint32_t new_index = new_element->index;
+    if(new_index < head->index){
+       new_element->next = head;
+       head=  new_element;
+       return head;
+    }
+    else{
+         prev=  head;
+         while(prev->next != NULL){
+            curr = prev->next;
+            if(new_index < curr->index){
+               prev->next = new_element;
+               new_element->next = curr; 
+               return head;
+            }
+            prev = curr;
+          }
+          if(prev->next== NULL){
+               prev->next = new_element;
+               new_element->next = NULL;
+         }
+    }
 }
 
 // Reverses the order of the list starting at `head` and returns a pointer to
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
-
-	return head;
+	list_t* first, sec, third;
+	if(head->next == NULL){
+		return head;
+   	}
+   	else{
+      		sec = head->next;
+      		first = head;
+		 if(sec->next == NULL){
+		         sec->next=  first;
+         		first->next = NULL;
+        		 head = sec;
+        		 return head; 
+      		}
+      		else{
+        		 while(sec->next != NULL){
+			    third = sec->next;
+        		    sec->next= first;
+			    sec = third;
+            		    first = sec;
+			  }
+         		sec->next=  first;
+		  	head->next = NULL;
+         		head = sec;
+         		return head;
+     		}
+   	}
 }
 
